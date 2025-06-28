6207a4c8fb1dfe80fb0f92dadd6ada3fb83bd5d6
Eric Lee <ericleedaily@gmail.com>
2025-06-28 15:14:33 +0800

    更新 clean.sh

--- clean.sh
+++ clean.sh
@@ -1,47 +1,33 @@
-#!/usr/bin/env bash
-#
-# https://github.com/P3TERX/aria2.conf
-# File name：clean.sh
-# Description: Remove redundant files after Aria2 download is complete
-# Version: 3.0
-#
-# Copyright (c) 2018-2021 P3TERX <https://p3terx.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+#!/bin/bash
 
-CHECK_CORE_FILE() {
-    CORE_FILE="$(dirname $0)/core"
-    if [[ -f "${CORE_FILE}" ]]; then
-        . "${CORE_FILE}"
-    else
-        echo "!!! core file does not exist !!!"
-        exit 1
-    fi
-}
+CONFIG_FILE="/data/data/com.termux/files/home/aria2/aria2.conf"
 
-CHECK_CORE_FILE "$@"
-CHECK_PARAMETER "$@"
-CHECK_FILE_NUM
-CHECK_SCRIPT_CONF
-GET_TASK_INFO
-GET_DOWNLOAD_DIR
-CONVERSION_PATH
-CLEAN_UP
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "错误：配置文件 $CONFIG_FILE 不存在"
+    exit 1
+fi
+
+DOWNLOAD_DIR=$(grep '^dir=' "$CONFIG_FILE" | cut -d'=' -f2)
+
+if [ -z "$DOWNLOAD_DIR" ]; then
+    echo "错误：无法从配置文件中读取下载目录"
+    exit 1
+fi
+
+if [ ! -d "$DOWNLOAD_DIR" ]; then
+    echo "错误：下载目录 $DOWNLOAD_DIR 不存在"
+    exit 1
+fi
+
+cd "$DOWNLOAD_DIR" || exit 1
+
+find . -type f -name "*.aria2" -delete
+
+if [ $? -eq 0 ]; then
+    echo "已成功清理所有.aria2文件"
+else
+    echo "清理.aria2文件时发生错误"
+    exit 1
+fi
+
 exit 0
